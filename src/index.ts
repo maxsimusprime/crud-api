f33e87ade7da1bc62f499f78de70564f2c7cccfb
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-02-12 14:49:42 +0300

    feat: add initial index.ts structure

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import { createServer } from "node:http";
+import { argv } from "node:process";
+
+const PORT = Number(process.env.PORT) || 4000;
+const HOST = process.env.HOST || "localhost";
+
+const args = argv.slice(2);
+
+const isMulti = !!args.find((arg) => arg === "--multi");
+
+if (!isMulti) {
+  const server = createServer((request, response) => {
+    try {
+      const { method } = request;
+  
+      switch (method) {
+        case "GET":
+          break;
+        case "POST":
+          break;
+        case "PUT":
+          break;
+        case "DELETE":
+          break;
+        default:
+          response.statusCode = 405;
+          response.statusMessage = "Invalid API request method";
+          response.end();
+          break;
+      }
+    } catch (error) {
+      response.statusCode = 500;
+      response.statusMessage = "Internal Server Error";
+      response.end();
+    }
+    
+  });
+  
+  server.listen(PORT, HOST, () => {
+    console.log(`Listen on port ${PORT}`);
+  });
+}
